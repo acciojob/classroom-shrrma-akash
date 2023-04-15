a2bb72136e6aacbbc915594fe15178313f5097c5
Akash Sharma <akash.bohareji@gmail.com>
2023-04-15 18:56:58 +0530

    Update StudentService.java

--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -9,41 +9,39 @@ public class StudentService {
     @Autowired
     StudentRepository student_repo;
     public void addStudent(Student student){
-        student_repo.addStudent(student);
+    student_repo.addStudent(student);
     }
 
     public void addTeacher(Teacher teacher){
-        student_repo.addTeacher(teacher);
+    student_repo.addTeacher(teacher);
     }
 
     public void addStudentTeacherPair(String student,String teacher){
-        student_repo.addStudentTeacherPair(student,teacher);
+    student_repo.addStudentTeacherPair(student,teacher);
     }
 
     public Student getStudentByName(String studentName ){
-        return student_repo.getStudentByName(studentName);
+    return student_repo.getStudentByName(studentName);
     }
 
     public Teacher getTeacherByName(String teacherName){
-        return student_repo.getTeacherByName(teacherName);
+    return student_repo.getTeacherByName(teacherName);
     }
 
     public List<String> getStudentsByTeacherName(String teacherName){
-        return student_repo.getStudentsByTeacherName(teacherName);
+    return student_repo.getStudentsByTeacherName(teacherName);
     }
 
     public  List<String> allStudent(){
-        return student_repo.getAllStudents();
+    return student_repo.getAllStudents();
     }
 
     public void  deleteTeacherByName(String teacherName){
-        student_repo.delete_teacher_by_name(teacherName);
+     student_repo.delete_teacher_by_name(teacherName);
 
     }
 
     public void deleteAllTeachers(){
-        deleteAllTeachers();
+    deleteAllTeachers();
     }
-
-
 }
